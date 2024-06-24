ccb700ab9635cd16867127288cea2c7cd8b9db69
ST10378552 <128582121+ST10378552@users.noreply.github.com>
2024-06-24 15:53:19 +0200

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using KhumaloWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace KhumaloWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Products> ProductsDetails { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
